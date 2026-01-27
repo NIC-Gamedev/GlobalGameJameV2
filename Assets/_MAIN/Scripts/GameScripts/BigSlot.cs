c2d620a957c18d7a34195e0be01b70a0c8f34e45
Rin <playmurka86@gmail.com>
2026-01-27 20:33:04 +0500

    Characters Aded

--- Assets/_MAIN/Scripts/GameScripts/BigSlot.cs
+++ Assets/_MAIN/Scripts/GameScripts/BigSlot.cs
@@ -2,9 +2,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+[SerializeField]
+public struct Characters
+{
+    public Sprite portait;
+    [TextArea]
+    public string text;
+}
 public class BigSlot : Slot
 {
-
+    [SerializeField] Characters characters;
     public override void OnDrop(PointerEventData eventData)
     {
         base.OnDrop(eventData);
