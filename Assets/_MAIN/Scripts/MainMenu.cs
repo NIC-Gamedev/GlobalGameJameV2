d2f251df8798d06ed72baef3b59fcb4cd325abad
Rindik <109646878+NotRindik@users.noreply.github.com>
2026-01-28 12:31:04 +0500

    MainMeny

--- Assets/_MAIN/Scripts/MainMenu.cs
+++ Assets/_MAIN/Scripts/MainMenu.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
-using UnityEngine.Device;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Audio")]
+    [SerializeField] private Slider volumeSlider, sfxSlider;
+    [SerializeField] private AudioMixer audioMixer;
 
-    public void changeScene(int a)
+    private const string VolumeKey = "Music";
+    private const string SFXKey = "SFX";
+
+    private void Awake()
     {
-        SceneManager.LoadScene(a);
+        float volume = PlayerPrefs.GetFloat(VolumeKey, 0.5f);
+        float sfxvol = PlayerPrefs.GetFloat(SFXKey, 0.5f);
+
+        SetMixer(VolumeKey, volume);
+        SetMixer(SFXKey, sfxvol);
+
+        volumeSlider.value = volume;
+        sfxSlider.value = sfxvol;
+
+        volumeSlider.onValueChanged.AddListener(v =>
+        {
+            PlayerPrefs.SetFloat(VolumeKey, v);
+            SetMixer(VolumeKey, v);
+        });
+
+        sfxSlider.onValueChanged.AddListener(v =>
+        {
+            PlayerPrefs.SetFloat(SFXKey, v);
+            SetMixer(SFXKey, v);
+        });
     }
+
+    void SetMixer(string key, float value)
+    {
+        float db = Mathf.Lerp(-80f, 0f, value);
+        audioMixer.SetFloat(key, db);
+    }
+
+    public void ChangeScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
     public void ExitGame()
     {
-        UnityEngine.Application.Quit();
+        Application.Quit();
     }
 }
